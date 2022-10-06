faf999237ad89fe0fc6ec287b4112f9da39de192
Joseph Oladeji <73621296+indexjoseph@users.noreply.github.com>
2022-10-06 19:41:14 -0400

    Update HashString.py

--- HashString.py
+++ HashString.py
@@ -2,18 +2,15 @@ import sys
 from Crypto.Hash import MD5, SHA1, SHA256
 
 def md5_hash(text):
-    md5_hash_output = MD5.new()
-    md5_hash_output.update(text)
+    md5_hash_output = MD5.new(text)
     return md5_hash_output.digest()
 
 def sha1_hash(text):
-    sha1_hash_output = SHA1.new()
-    sha1_hash_output.update(text)
+    sha1_hash_output = SHA1.new(text)
     return sha1_hash_output.digest()
 
 def sha256_hash(text):
-    sha256_hash_output = SHA256.new()
-    sha256_hash_output.update(text)
+    sha256_hash_output = SHA256.new(text)
     return sha256_hash_output.digest()
 
 def readFile(filename):
