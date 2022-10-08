1233140949e1366c1e4dc0123a1c82a2d7dae55f
Joseph Oladeji <73621296+indexjoseph@users.noreply.github.com>
2022-10-07 21:48:43 -0400

    Update mdtest.c

--- mdtest.c
+++ mdtest.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[]){
         exit(1);
     }
 
-    mdctx = EVP_MD_CTX_new(_);
+    mdctx = EVP_MD_CTX_new();
     EVP_DigestInit_ex(mdctx,md, NULL);
     EVP_DigestUpdate(mdctx, mess1, strlen(mess1) );
     EVP_DigestUpdate(mdctx, mess2, strlen(mess2) );
